739767f4d1f4d515a7d28dbe3eb0dafc4d8a0cf0
Red-Five <Red-Five@users.noreply.github.com>
2019-04-05 15:01:40 -0700

    Update

--- kube-bench-node-scan.sh
+++ kube-bench-node-scan.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
-# This script is unlicenced, however it uses kube-bench which is a Third Party open source
-# project covered under the Apache Licence.
+
+# Utility script to run kube-bench on GKE
 # Ref. https://github.com/aquasecurity/kube-bench
 # Ref. https://github.com/aquasecurity/kube-bench/blob/master/LICENSE
 
+# Fail fast
+set -e
+
+# Log cmds to console
 set -x
 
+# Clone kube-bench master branch locally
 git clone --single-branch --branch=master https://github.com/aquasecurity/kube-bench.git
 
-# TODO Point to the Cluster to be analysed
-CLUSTER= #e.g. standard-cluster-1
-ZONE= 	 #e.g. us-central1-a
+# USER TODO Point to the Cluster to be analysed
+CLUSTER=#e.g. standard-cluster-1
+REGION=#e.g. us-central1
 
-gcloud config set compute/zone ${ZONE}
-gcloud config set container/cluster ${CLUSTER}
-gcloud container clusters get-credentials ${CLUSTER}
+gcloud container clusters get-credentials ${CLUSTER} --region=${REGION}
+
+# Deploy kube-bench to cluster
 kubectl apply -f kube-bench/job-node.yaml
 
-echo 'sleep 60 to allow time for job to complete'
-sleep 60
+echo 'sleep 30 to allow time for job to complete'
+sleep 30
 
+# Identify the kube-bench pod id
 POD=$(kubectl get pod -l job-name=kube-bench-node -o name)
+
+# Download kube-bench pod logs, they contain the inspection report
 kubectl logs ${POD} > kube-bench-node-scan.log
 
 # Cleanup
 kubectl delete -f kube-bench/job-node.yaml
 rm -rf kube-bench
 
+# Unset logs cmds to console
 set +x
+
+# Unset fail fast
+set +e
